948a7a9d7ec8e071f68fd6e37db9420eb5cf6f85
aSknkn <omsksaa@gmail.com>
2026-06-24 01:28:39 +0600

    feat: task01

---
+++ task01/task01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomExtention
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (input.Length == 0) return false;
+            else {
+            input = input.ToLower();
+            string output="";
+            for (int i=0; i<input.Length; i++)
+            {
+                if (!char.IsPunctuation(input[i]) && !char.IsWhiteSpace(input[i])) output+=input[i];
+            }
+            
+            return output.SequenceEqual(output.Reverse());
+            }
+        }
+    }
+}
