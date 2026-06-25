7f237e6b027a6ba8f88baecfbf59f6f6ea5af894
aSknkn <omsksaa@gmail.com>
2026-06-25 11:27:50 +0600

    feat: task02

---
+++ task02/task02.cs
@@ -0,0 +1,33 @@
+﻿namespace task02;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Faculty { get; set; }
+    public List<int> Grades { get; set; }
+
+}
+
+public class StudentService
+{
+    private readonly List<Student> _students;
+
+    public StudentService(List<Student> students) => _students = students;
+
+    public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+    => _students.Where(student => student.Faculty == faculty);
+
+    public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade)
+    => _students.Where(student => student.Grades.Sum()/student.Grades.Count >= minAverageGrade);
+
+    public IEnumerable<Student> GetStudentsOrderedByName()
+        => _students.OrderBy(student => student.Name);
+
+    public ILookup<string, Student> GroupStudentsByFaculty()
+        => _students.ToLookup(student => student.Faculty);
+
+    public string GetFacultyWithHighestAverageGrade()
+      => _students.GroupBy(student => student.Faculty).OrderBy(group => group.Average(student => student.Grades.Average()))
+         .Select(grup => grup.Key)
+          .Last() ?? string.Empty;
+}
